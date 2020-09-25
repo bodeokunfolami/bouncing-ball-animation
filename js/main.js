0c61bbf279de42e84c7a5c0247a8c3fe149115f6
oyebodefreeman <bodokunfolami@gmail.com>
2020-09-25 18:42:35 +0100

    bouncing only

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// const progress = document.querySelector('.overlay');
-
-// let counter = 0;
-// let timer;
-
-// function step(timestamp) {
-//     counter++;
-//     progress.style.width = (counter / 100) + "%";
-
-//     if (counter < (100 * 100)) {
-//         requestAnimationFrame(step);
-//     }
-// }
-
-
-
-// requestAnimationFrame(step);
-
 const canvas = document.querySelector('#canvas');
 const ball = document.querySelector('#ball');
 const ctx = canvas.getContext('2d');
@@ -29,34 +11,6 @@ let moveX = 0;
 let speedX = 5;
 let speedY = 4;
 
-
-function onKeyUp(e) {
-    if (e.key === "ArrowRight") {
-        console.log(moveX);
-        if (moveX + ballSize < canvas.width) {
-            moveX += 10;
-            update(moveX, moveY);
-        }
-    } else if (e.key === "ArrowLeft") {
-
-        if (moveX > 0) {
-            moveX -= 10;
-            update(moveX, moveY);
-        }
-    } else if (e.key === "ArrowUp") {
-        if (moveY > 0) {
-            moveY -= 10;
-            update(moveX, moveY);
-        }
-
-    } else if (e.key === "ArrowDown") {
-        if (moveY + ballSize < canvas.height) {
-            moveY += 10;
-            update(moveX, moveY);
-        }
-
-    }
-}
 
 function update() {
 
@@ -83,8 +37,6 @@ function update() {
 
 
 }
-
-document.addEventListener('keydown', onKeyUp);
 
 
 requestAnimationFrame(update);
